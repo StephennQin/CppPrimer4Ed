a5a8bf042d9b6a92d2029f6f548dc2b9fa98afdf
姜景宁(Jake Jiang) <jakejiangjn@users.noreply.github.com>
2014-03-20 13:49:49 +0800

    git@github.com:jakejiangjn/CppPrimer4Ed.git

---
+++ Ch13_Definition.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+#include <stdio.h>
+
+class Employee
+{
+public:
+	// constructor
+	Employee():name("NoName"), id(counter)
+		{ ++ counter; }
+	Employee( std::string EmName ):name(EmName), id(counter)
+		{ ++ counter; }
+	Employee( const Employee& other ):name(other.name), id(counter)
+		{ ++ counter; }
+	Employee& operator= (const Employee& other)
+	{
+		name = other.name;
+		return *this;
+	}
+private:
+	std::string name;
+	unsigned int id;
+	static int counter;
+};
+
+int Employee::counter = -1;
+
+class Employer
+{
+public:
+	Employer():name("NoName"), id(counter)
+		{ setID();
+		}
+	Employer( std::string EmName ):name(EmName), id(counter)
+		{ setID(); }
+	Employer( const Employer& other ):name(other.name), id(counter)
+		{ setID(); }
+	Employer& operator= (const Employer& other)
+	{
+		name = other.name;
+		return *this;
+	}
+	~Employer();
+private:
+	std::string name;
+	std::string id;
+	static int counter;
+	void setID()
+	{
+		++counter;
+		id = "Employer-";
+		if ( counter < 10 )
+			id += "000";
+		else if ( counter < 100 )
+			id += "00";
+		else if ( counter < 1000 )
+			id += "0";
+		else
+			std::cerr << "No valid Employer ID input!" << std::endl;
+		char buffer[5];
+		itoa( counter, buffer, 10 );
+		id += buffer;
+		// stringstream ss;
+		// ss << counter;
+		// id += ss.str();
+	}
+};
+
+int Employer::counter = -1;
